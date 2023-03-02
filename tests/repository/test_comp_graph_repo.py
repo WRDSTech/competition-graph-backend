d4f4e663fc3c1f6c7718441d247a8d7be34decbe
Jiawei Huang <jwhuang11235@gmail.com>
2023-03-02 09:03:05 +0000

    Unique relationship id

--- tests/repository/test_comp_graph_repo.py
+++ tests/repository/test_comp_graph_repo.py
@@ -15,7 +15,9 @@ def test_get_surrounding_node_by_center(comp_graph_repo):
     partial_graph = comp_graph_repo.get_surrounding_node_by_center(6, 3)
     assert len(partial_graph.links) == 3
     assert len(partial_graph.nodes) == 4
-    assert Entity(id="8", name="Disney") in partial_graph.nodes
-    assert Entity(id="9", name="Standard Chartered Bank") not in partial_graph.nodes
-    assert EntityRelation(id="1", category="product", source="6", target="5") in partial_graph.links
-    assert EntityRelation(id="1", category="product", source="5", target="6") not in partial_graph.links
+    assert Entity(id=8, name="Disney") in partial_graph.nodes
+    assert Entity(id=9, name="Standard Chartered Bank") not in partial_graph.nodes
+    assert EntityRelation(id=3, category="product", source=6, target=5) in partial_graph.links
+    assert EntityRelation(id=3, category="product", source=5, target=6) not in partial_graph.links
+    assert EntityRelation(id=5, category="competition", source=5, target=7) in partial_graph.links
+    assert EntityRelation(id=5, category="competition", source=7, target=5) not in partial_graph.links
